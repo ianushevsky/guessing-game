d11dbb27aa1ea7463453289b0d0526fb81365c52
Igor Chmil <ian.i.chmil@gmail.com>
2021-02-08 18:29:07 -0600

    changes

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-
+var count=5
 var computers_guess=Math.floor(Math.random() * 10) + 1;
-function checkguess(){
+function checkguess(){document.getElementById("user_lives").innerHTML="You have "+count+"lives remaining"
   var c=document.getElementById("num_input").value;
+  if (c==computers_guess){alert ("right")}
+  else {
+    alert("wrong")
+    count-=1
+    if (count==0) window.location.href="https://www.google.com"
+    document.getElementById("user_lives").innerHTML="You have "+count+" lives remaining"
+  }
+
 }
